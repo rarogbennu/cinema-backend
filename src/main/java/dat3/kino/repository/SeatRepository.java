eb6b64a018c452da4d70ea7a1cfe0982244744ad
svdf18 <simonvandeurs@hotmail.com>
2024-03-20 08:37:37 +0100

    KINO-74 get seats by screen id + init screenings updated

--- src/main/java/dat3/kino/repository/SeatRepository.java
+++ src/main/java/dat3/kino/repository/SeatRepository.java
@@ -3,5 +3,8 @@ package dat3.kino.repository;
 import dat3.kino.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatRepository extends JpaRepository <Seat,Integer> {
+    List<Seat> findByScreenId(int screenId);
 }
